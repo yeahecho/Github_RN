c8c27eb0a5e7cfa3859a604d1e1ad930962e63b7
yeahecho <yeahecho@gmail.com>
2019-02-19 15:16:30 -0500

    second

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import WelcomePage from './js/page/WelcomePage';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
